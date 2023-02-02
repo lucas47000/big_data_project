7275d8bae086596bc3916af78d2fd1613d519f62
Lucas <garcia@et.esiea.fr>
2023-02-02 21:51:31 +0100

    Version 2 inccluant HDFS (besoin d'une machine incluant HADOOP).

--- covid19_analysis_app.py
+++ covid19_analysis_app.py
@@ -10,13 +10,13 @@ with open(file_name, "wb") as f:
 
 print("Le fichier a été téléchargé avec succès sous le nom : " + file_name)
 
-# 1. Initialiser SparkSession avec la configuration pour HDFS
+# Initialise SparkSession avec la configuration pour HDFS
 spark = SparkSession.builder.appName("Covid19DataAnalysis").config("spark.hadoop.fs.defaultFS", "hdfs://localhost:9000").getOrCreate()
 
-# 2. Copier le fichier CSV téléchargé dans HDFS
+# Copie le fichier CSV téléchargé dans HDFS
 subprocess.run(["hdfs", "dfs", "-put", "covid_19_data.csv", "/"])
 
-# 3. Lire le fichier CSV à partir de HDFS
+# 3. Lis le fichier CSV à partir de HDFS
 df = spark.read.csv("hdfs://localhost:9000/covid_19_data.csv", header=True, inferSchema=True)
 
 # Extrait et affiche le nombre de cas total par pays
@@ -24,17 +24,17 @@ total_cases = df.groupBy("Country").agg({"Confirmed": "sum"})
 print("nombre de cas total par pays : ")
 total_cases.show()
 
-# Afficher le nombre total de morts pour chaque pays
+# Affiche le nombre total de morts pour chaque pays
 total_deaths = df.groupBy("Country").agg({"Deaths": "sum"})
 print("Total Deaths by Country:")
 total_deaths.show()
 
-# Afficher le nombre total de cas confirmés et de morts pour chaque pays
+# Affiche le nombre total de cas confirmés et de morts pour chaque pays
 total_cases_and_deaths = df.groupBy("Country").agg({"Confirmed": "sum", "Deaths": "sum"})
 print("Total Confirmed Cases and Deaths by Country:")
 total_cases_and_deaths.show()
 
-# Afficher le taux de mortalité moyen pour chaque pays (en pourcentage)
+# Affiche le taux de mortalité moyen pour chaque pays (en pourcentage)
 average_mortality_rate = df.groupBy("Country").agg({"Deaths": "avg", "Confirmed": "avg"})
 average_mortality_rate = average_mortality_rate.withColumn("Mortality Rate", average_mortality_rate["avg(Deaths)"] / average_mortality_rate["avg(Confirmed)"] * 100)
 average_mortality_rate = average_mortality_rate.drop("avg(Deaths)", "avg(Confirmed)")
